3c8edc72357d344e00bd62301168fe2545b792cf
Brennon York <brennon.york@gmail.com>
2019-04-14 01:09:49 -0700

    fixed load-theme to update the symlink and restart ghost only if necessary

--- bin/load-theme.sh
+++ bin/load-theme.sh
@@ -11,13 +11,21 @@ ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
 # Source in our constants. We always start our scripts with this :)
 source "${ROOT}/bin/constants.sh"
 
+TARGET_DIR="${WEBSITE_DIR}/current/content/themes"
+SYMLINK_FILE="${WEBSITE_DIR}/content/themes/$1"
+
 # Check if the passed in string is a valid directory in the theme directory
 if [ -d "${THEME_DIR}/$1" ]; then
     # If so copy the given folder into the current themes directory
-    TARGET_DIR="${WEBSITE_DIR}/current/content/themes"
-    cp -fr "${THEME_DIR}/$1" "${TARGET_DIR}/."
-    # Then symlink it to general 'themes' directory
-    ln -s "${TARGET_DIR}/$1" "${WEBSITE_DIR}/content/themes/$1" 2>/dev/null
+    cp -r "${THEME_DIR}/$1" "${TARGET_DIR}/."
+
+    # Check if the symlink doesn't exist
+    if [ ! -L "${SYMLINK_FILE}" ]; then
+	# Then symlink it to general 'themes' directory
+	ln -s "${TARGET_DIR}/$1" "${SYMLINK_FILE}"
+	# And restart the `ghost` server
+	cd "${WEBSITE_DIR}" && ghost restart
+    fi
 else
     echo "ERROR: Cannot find that theme, are you sure you spelled it right?"
     exit 3
